00a5afa8816d7629dee3d9ab0be322dba22e3cd5
SagarSunny00 <sagarsunny1029@gmail.com>
2025-03-11 13:46:48 +0530

    initial

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
 from flask import render_template , Flask 
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('signup.html')
+def hello_world():
+    return render_template('templates/hom.html')
 
-@app.route('/machine-monitoring')
-def machine_monitoring():
-    # Sample machine data (replace with actual data from your machines)
-    machine1_status = "Running"
-    machine1_temperature = 25
-    machine1_pressure = 10
+# @app.route('/')
+# def index():
+#     return render_template('templates/signup.html')
 
-    machine2_status = "Idle"
-    machine2_temperature = 20
-    machine2_pressure = 5
+# @app.route('/machine-monitoring')
+# def machine_monitoring():
+#     # Sample machine data (replace with actual data from your machines)
+#     machine1_status = "Running"
+#     machine1_temperature = 25
+#     machine1_pressure = 10
 
-    return render_template('machine_monitoring.html',
-                               machine1_status=machine1_status,
-                               machine1_temperature=machine1_temperature,
-                               machine1_pressure=machine1_pressure,
-                               machine2_status=machine2_status,
-                               machine2_temperature=machine2_temperature,
-                               machine2_pressure=machine2_pressure)
+#     machine2_status = "Idle"
+#     machine2_temperature = 20
+#     machine2_pressure = 5
+
+#     return render_template('machine_monitoring.html',
+#                                machine1_status=machine1_status,
+#                                machine1_temperature=machine1_temperature,
+#                                machine1_pressure=machine1_pressure,
+#                                machine2_status=machine2_status,
+#                                machine2_temperature=machine2_temperature,
+#                                machine2_pressure=machine2_pressure)
 
 if __name__ == '__main__':
     app.run(debug=True)
